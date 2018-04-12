b85ba37b046058baeb54d950d3e10303d9417182
swarrier16 <swarrier16@gmail.com>
2018-04-12 01:06:53 -0400

    final working todo app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from "./components/header/";
-import TaskBar from "./components/taskbar/";
-import TaskList from "./components/tasklist/";
+import Header from "./components/header";
+import TaskBar from "./components/taskbar";
+import TaskList from "./components/tasklist";
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from "./reducers/";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+let store = createStore(reducers);
+
+
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+
+    ,
+    document.getElementById('root'));
+
